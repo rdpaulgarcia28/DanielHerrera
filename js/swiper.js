e1b35144df33e252e44cbe1c90624cd4206c5201
Paul <rdpaulgarcia@gmail.com>
2024-02-19 21:35:30 -0600

    19022024

---
+++ js/swiper.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper(".swiper_partherships", {
+  // Optional parameters
+  loop: true,
+  autoplay: {
+    dalay: 2000,
+  },
+
+  // If we need pagination
+  // pagination: {
+  //   el: ".swiper-pagination",
+  // },
+
+  // // Navigation arrows
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
+});
